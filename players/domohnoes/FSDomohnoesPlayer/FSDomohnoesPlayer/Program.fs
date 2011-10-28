e5b8f5ec437ce2a5de677dff293bc3238cb940aa
Harold <hhausman@gmail.com>
2011-10-28 17:32:45 -0600

    Beginning to rewrite the FSDomsPlayer.

--- players/domohnoes/FSDomohnoesPlayer/FSDomohnoesPlayer/Program.fs
+++ players/domohnoes/FSDomohnoesPlayer/FSDomohnoesPlayer/Program.fs
@@ -1,25 +1,63 @@
 ﻿//TODO: Factor the general Player-ness out of this into a resuable thing.
 
 open Newtonsoft.Json
+open System
 open System.Net.Sockets
 open System.Text
 open System.IO
 
 let utf8 = Encoding.UTF8
 
-let command (c:string) (a:string[]) =
+type CommandDetail =
+  | S of string
+  | L of Collections.Generic.List<int>
+type CommandDetails = Collections.Generic.Dictionary<String,CommandDetail>
+
+let getJson (d:CommandDetails) =
   let sb = StringBuilder()
-  use sw = new StringWriter(sb)
-  use j = new JsonTextWriter(sw)
+  use j = new JsonTextWriter(new StringWriter(sb))
   j.WriteStartObject()
-  j.WritePropertyName("command")
-  j.WriteValue(c)
-  j.WritePropertyName("args")
-  j.WriteStartArray()
-  Array.iter (fun (s:string) -> j.WriteValue(s)) a
-  j.WriteEnd()
+  for k in d.Keys do
+    j.WritePropertyName(k)
+    match d.[k] with
+      | S s -> j.WriteValue(s)
+      | L l -> j.WriteStartArray()
+               Seq.iter (fun (i:int) -> j.WriteValue(i)) l
+               j.WriteEnd()
   j.WriteEndObject()
   sb.ToString()
+
+type JSONSocket(c:TcpClient) =
+  member self.Command( t, (details:CommandDetails) ) =
+    details.Add("command",t)
+    self.SendData( details )
+
+  member self.Error( t, (details:CommandDetails) ) =
+    details.Add("error",t)
+    self.SendData( details )
+
+  member self.OnRecieve( f ) =
+    f (self.ReadData())
+
+  member self.SendData( data ) =
+    let b = utf8.GetBytes(getJson data)
+    let l = b.Length
+    let stream = c.GetStream()
+    let hb = BitConverter.GetBytes( int16 l )
+    let temp = hb.[0]
+    hb.[0] <- hb.[1]; hb.[1] <- temp
+    stream.Write( hb, 0, 2 )
+    stream.Write( b, 0, l)
+
+  member self.ReadData() =
+    let stream = c.GetStream()
+    let header = Array.create 2 ((byte)0)
+    stream.Read( header, 0, 2 ) |> ignore
+    let l = BitConverter.ToInt16( header, 0 )
+    let size = ((int)l)
+    let response = Array.create size ((byte)0)
+    let l = stream.Read( response, 0, size )
+    String(utf8.GetChars( response, 0, size )).Trim(Array.create 1 ((char)0))
 
 let pukeJSON (s:string) =
   printfn "%O" s
@@ -31,23 +69,12 @@ let pukeJSON (s:string) =
 
 let gameType = "com.danceliquid.domohnoes"
 
-let streamWrite (stream:NetworkStream) (s:string) =
-  let b = utf8.GetBytes(s)
-  let l = b.Length
-  stream.Write(b, 0, l)
+let socket = new JSONSocket( new TcpClient("localhost", 54147) )
 
-let streamRead (stream:NetworkStream) =
-  let size = 2<<<16-1
-  let response = Array.create size ((byte)0)
-  let l = stream.Read( response, 0, size )
-  System.String(utf8.GetChars( response, 0, l ))
+let startGameData = new CommandDetails()
+startGameData.Add("gametype", S(gameType))
+startGameData.Add("nick", S("F# Doms Player"))
+socket.Command(S("start_game"), startGameData)
 
-let stream = (new TcpClient("localhost", 54147)).GetStream()
-
-let interact cmd =
-  streamWrite stream cmd
-  pukeJSON (streamRead stream)
-
-interact (command "gametype_supported" [| gameType |])
-interact (command "start_game" [| gameType |])
-interact (command "goodbye" [| |])
+// TODO: loop ;)
+socket.OnRecieve pukeJSON
